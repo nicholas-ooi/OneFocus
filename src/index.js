31238e75d943f67745117c579596ddb412b5b554
Nicholas Ooi <djfanaticx@gmail.com>
2016-11-21 20:21:44 -0800

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HeapSort from './App';
+import './index.css';
+
+
+ReactDOM.render(
+  <div>
+  < />
+  </div>,
+  document.getElementById('root')
+);
